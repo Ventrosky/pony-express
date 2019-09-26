9b2aef13de6665b2ba6d7a2976d25e5d48720842
Salvatore Ventrone <ventrosky@gmail.com>
2019-09-26 17:45:56 +0200

    Chapter 4
    Working with Request Bodies

--- routes/emails.js
+++ routes/emails.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const readBody = require('../lib/read-body');
+const generateId = require('../lib/generate-id');
 const emails = require('../fixtures/emails');
 
 const stringify = require('csv-stringify');
@@ -32,10 +34,37 @@ let getEmailRoute = (req, res) => {
     res.send(email);
 }
 
+let createEmailRoute = async (req, res) => {
+    let body = await readBody(req);
+    let newEmail = {...JSON.parse(body), id: generateId()};
+    emails.push(newEmail);
+    res.status(201);
+    res.send(newEmail);
+};
+
+let updateEmailRoute = async (req, res) => {
+    let body = await readBody(req);
+    let email = emails.find(email => email.id === req.params.id);
+    Object.assign(email, JSON.parse(body));
+    res.status(200);
+    res.send(email);
+};
+
+let deleteEmailRoute = (req, res) => {
+    let index = emails.findIndex(email => email.id === req.params.id);
+    emails.splice(index, 1);
+    res.sendStatus(204);
+};
 
 let emailsRouter = express.Router();
 
-emailsRouter.get('/', getEmailsRoute);
-emailsRouter.get('/:id', getEmailRoute);
+emailsRouter.route('/')
+    .get(getEmailsRoute)
+    .post(createEmailRoute);
+
+emailsRouter.route('/:id')
+    .get(getEmailRoute)
+    .patch(updateEmailRoute)
+    .delete(deleteEmailRoute);
 
 module.exports = emailsRouter;
